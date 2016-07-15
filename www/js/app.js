9fdf3671ad20d4355cbd55215b3d87e6140e151e
TusharSachde <tushar@wohlig.com>
2016-07-15 18:34:05 +0530

    Done Pushwoosh

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,37 @@
-// Ionic Starter App
+function initPushwoosh() {
+    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+    if (device.platform == "Android") {
+        console.log("Android");
+        registerPushwooshAndroid();
+    }
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
+    if (device.platform == "iPhone" || device.platform == "iOS") {
+        registerPushwooshIOS();
+    }
+
+    pushNotification.getLaunchNotification(
+        function(notification) {
+            if (notification != null) {
+                console.log(JSON.stringify(notification));
+            } else {
+                console.log("No launch notification");
+            }
+        }
+    );
+}
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    initPushwoosh();
   });
 })
 
